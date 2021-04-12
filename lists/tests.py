0a28ea0c132c0962bebcfe32fc11ec9dcc1326aa
Zhilal Yusya <zhilalyusya@gmail.com>
2021-04-12 15:07:09 +0200

    New URL and views to create a new list

--- lists/tests.py
+++ lists/tests.py
@@ -6,28 +6,27 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
+		
+
+class NewListTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_redirects_after_POST(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-	def test_only_saves_items_when_necessary(self):
-		self.client.get('/')
-		self.assertEqual(Item.objects.count(), 0)
 
 class ListViewTest(TestCase):
 
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
+
 	def test_diplays_all_list_items(self):
 		Item.objects.create(text='itemey 1')
 		Item.objects.create(text='itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,11 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
 	return render(request, 'home.html')
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
 	items = Item.objects.all()
