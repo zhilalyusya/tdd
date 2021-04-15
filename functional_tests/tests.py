043e21e9346c9b5756b07d3a3cafe2efb50fed04
Zhilal Yusya <zhilalyusya@gmail.com>
2021-04-15 15:55:48 +0200

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,7 +9,8 @@ MAX_WAIT = 10
 class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
-		self.browser = webdriver.Firefox()
+		# self.browser = webdriver.Firefox()
+		self.browser = webdriver.Chrome()
 
 	def tearDown(self):
 		self.browser.quit()
@@ -82,7 +83,8 @@ class NewVisitorTest(LiveServerTestCase):
 		## Use a new browser session to make sure that no information
 		## of Inong's is coming through from cookies etc
 		self.browser.quit()
-		self.browser = webdriver.Firefox()
+		# self.browser = webdriver.Firefox()
+		self.browser = webdriver.Chrome()
 
 		# Agam visits the home page. There is no sign of Inong's list
 		self.browser.get(self.live_server_url)
@@ -108,6 +110,30 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep 
+
+	def test_layout_and_styling(self):
+		# Inong goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
+
+		# She starts a new list and sees the input is nicely centered there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+			)
 
 
 
