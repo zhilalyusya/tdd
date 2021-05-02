686bb894f683f3644e9d7b1ce162c5bf6ec02817
Zhilal Yusya <zhilalyusya@gmail.com>
2021-05-02 18:26:12 +0200

    Save data within forms, create custom save class to do this

--- lists/forms.py
+++ lists/forms.py
@@ -14,8 +14,11 @@ class ItemForm(forms.models.ModelForm):
 				'placeholder': 'Enter a to-do item',
 				'class': 'form-control input-lg',
 				})
-
 		}
 		error_messages = {
 			'text': {'required': EMPTY_ITEM_ERROR}
 		}
+
+	def save(self, for_list):
+		self.instance.list = for_list
+		return super().save()
