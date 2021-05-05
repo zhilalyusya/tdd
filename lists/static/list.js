4e8ca80b99868517763bd7ae984e1712db4e40b9
Zhilal Yusya <zhilalyusya@gmail.com>
2021-05-05 14:22:42 +0200

    add jquery, qunit tests, list.js with keypresslisteners

---
+++ lists/static/list.js
@@ -0,0 +1,9 @@
+$('input[name="text"]').on('keypress', function () {
+	$('.has-error').hide();
+});
+
+var initialize = function () {
+	$('input[name="text"]').on('keypress', function () {
+		$('.has-error').hide();
+	});
+};
